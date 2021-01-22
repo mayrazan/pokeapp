d04584846fcd4deca74f2e15f0aab46895ab35b9
acsbrito <anac.brito@yahoo.com.br>
2021-01-22 20:45:46 -0300

    coloquei o pacote

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "./components/Button/button";
+import Loading from "react-loading";
 
 
 function App() {
   function capturar() {
-       
-     console.log("Capturado!")
-
+    console.log("Capturado!")
   }
+
   return (
     <div className="App">
       <Button onClick={capturar}></Button>
+      <Loading></Loading>
     </div>
   );
+
 }
 
 export default App;
