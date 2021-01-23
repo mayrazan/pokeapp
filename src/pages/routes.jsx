fa1858080cf8dc7ec7419745a6abf2f939bb370b
mayrazan <mayramanchein@yahoo.com.br>
2021-01-23 03:08:27 -0300

    Adicionando pagina meus pokemons

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,12 +1,13 @@
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { PokemonCard } from "../components/Card/PokemonCard";
+import { MyPokemons } from "../components/MyPokemons/MyPokemons";
 
 export function Routes() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={PokemonCard} exact />
-        {/* <Route path="/my-pokemons" component={MyPokemons} exact /> */}
+        <Route path="/my-pokemons" component={MyPokemons} exact />
       </Switch>
     </Router>
   );
